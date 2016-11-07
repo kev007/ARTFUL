067933a1fd55967eb22e76397e3c66ba29a7177e
Kevin Shrestha <kev_shrestha@hotmail.com>
2016-11-07 23:09:42 +0100

    #2 fixed updateDatabase function. improved performance. added performance metrics.

--- leipzigCorporaParser/src/fmdir/Main.java
+++ leipzigCorporaParser/src/fmdir/Main.java
@@ -14,11 +14,21 @@ public class Main {
     //Global properties
     public static Properties prop = new Properties();
     public static int id = 0;
-    public static String driverName= "org.sqlite.JDBC";
+    public static final String driverName= "org.sqlite.JDBC";
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        System.out.println("Starting time: " + new Date());
+        System.out.println(ANSI_CYAN + "Starting time: " + new Date() + ANSI_RESET);
 
         readConfig();
 
@@ -53,25 +63,25 @@ public class Main {
             String language = parseLanguage(fileName);
 
             temp++;
-            System.out.println("(" + temp + "/" + allFilePaths.size() + ") - " +  year + " " + language + ": " + wordFreq.size() + " words");
+            System.out.println(ANSI_BLUE + "(" + temp + "/" + allFilePaths.size() + ") - " +  year + " " + language + ": " + wordFreq.size() + " words" + ANSI_RESET);
 //            System.out.println("test frequency: " + wordFreq.get("!"));
 
 //            fillDatabase(wordFreq, year, language);
             updateDatabase(wordFreq, year, language);
         }
 
-        System.out.println("End time: " + new Date());
-        System.out.println("Duration: " + (System.currentTimeMillis() - startTime)/1000 + " seconds");
+        System.out.println(ANSI_CYAN + "End time: " + new Date() + ANSI_RESET);
+        System.out.println(ANSI_CYAN + "Duration: " + (float)(System.currentTimeMillis() - startTime)/1000 + " seconds" + ANSI_RESET);
 
 
-        printDB(5);
+//        TESTprintDB(5);
     }
 
     /**
      * DEBUG function
      * @param rowCount
      */
-    private static void printDB(int rowCount) {
+    private static void TESTprintDB(int rowCount) {
         System.out.println("Testing database read: ");
         try {
             Class.forName(driverName).newInstance();
@@ -117,30 +127,62 @@ public class Main {
 
     private static void updateDatabase(HashMap<String, Integer> wordFreq, String year, String language) {
         try {
+            long updateStart = System.currentTimeMillis();
+
             Class.forName(driverName).newInstance();
             Connection connection = DriverManager.getConnection("jdbc:sqlite:" + prop.getProperty("dbPath"));
 
-//            Statement s = connection.createStatement();
-//            ResultSet rs = s.executeQuery("SELECT language, word, " + year + " FROM words WHERE language='" + language + "';");
+            Statement s = connection.createStatement();
+            ResultSet rs = s.executeQuery("SELECT word, language, " + year + " FROM words WHERE language='" + language + "';");
+//            int columnCount = rs.getMetaData().getColumnCount();
+//            int rowCount = 3;
+//            int temp = 0;
+//            while (rs.next() && temp<rowCount){
+//                temp++;
+//                String allColumns = "";
+//                for (int i=1; i<=columnCount; i++) allColumns += rs.getString(i) + "\t";
+//                System.out.println(allColumns);
+//            }
 
             PreparedStatement update = connection.prepareStatement("update words set " + year + " = ?1 WHERE word = ?2 and language = ?3");
 
-            for (Object kvPair : wordFreq.entrySet()) {
-                Map.Entry pair = (Map.Entry) kvPair;
-                String word = pair.getKey().toString();
-                int freq = (int) pair.getValue();
+            int searched = 0;
+            int found = 0;
+            while (rs.next() && searched<9){
+                searched++;
 
-                update.setInt(1, freq);
-                update.setString(2, word);
-                update.setString(3, language);
-                update.addBatch();
+                String word = rs.getString(1);
+
+                if (wordFreq.containsKey(word)) {
+                    found++;
+
+                    update.setInt(1, wordFreq.get(word));
+                    update.setString(2, word);
+                    update.setString(3, language);
+                    update.addBatch();
+                }
             }
+//            for (Object kvPair : wordFreq.entrySet()) {
+//                Map.Entry pair = (Map.Entry) kvPair;
+//                String word = pair.getKey().toString();
+//                int freq = (int) pair.getValue();
+//
+//                update.setInt(1, freq);
+//                update.setString(2, word);
+//                update.setString(3, language);
+//                update.addBatch();
+//            }
+
+            long commitStart = System.currentTimeMillis();
+            System.out.println(searched + " words queried and compared in\t" + (float)(commitStart-updateStart)/1000 + " seconds");
 
             connection.setAutoCommit(false);
             update.executeBatch();
 
             connection.commit();
             connection.close();
+
+            System.out.println(found + " word frequencies updated in\t" + (float)(System.currentTimeMillis()-commitStart)/1000 + " seconds");
         } catch (Exception e) {
             e.printStackTrace();
         }
