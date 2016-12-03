e107321ca58bd8e5c2f238c55ebd0654a9f4fac5
Peter Nancke <peter.nancke@gmx.de>
2016-12-03 19:10:06 +0100

    Zoom out map on default

--- server/src/main/webapp/resources/app.js
+++ server/src/main/webapp/resources/app.js
@@ -1,5 +1,5 @@
 function initLeafletMap() {
-    var map = L.map('mapid').setView([51.505, -0.09], 13);
+    var map = L.map('mapid').setView([51.505, -0.09], 3);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
         maxZoom: 18,
