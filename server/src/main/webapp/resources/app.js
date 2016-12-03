eebee1ce0f62a08dae40fcd966cb95590c64cefc
Peter Nancke <peter.nancke@gmx.de>
2016-12-03 19:06:37 +0100

    Colorize Countries

--- server/src/main/webapp/resources/app.js
+++ server/src/main/webapp/resources/app.js
@@ -1,38 +1,46 @@
 function initLeafletMap() {
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var map = L.map('mapid').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        id: 'mapbox.streets'
-    }).addTo(mymap);
-
-    L.marker([51.5, -0.09]).addTo(mymap)
-        .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-    L.circle([51.508, -0.11], 500, {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5
-    }).addTo(mymap).bindPopup("I am a circle.");
-
-    L.polygon([
-        [51.509, -0.08],
-        [51.503, -0.06],
-        [51.51, -0.047]
-    ]).addTo(mymap).bindPopup("I am a polygon.");
-
+        id: 'mapbox.light'
+    }).addTo(map);
 
     var popup = L.popup();
+
+    L.geoJson(countryData, {style: style}).addTo(map);
 
     function onMapClick(e) {
         popup
             .setLatLng(e.latlng)
             .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(mymap);
+            .openOn(map);
     }
 
-    mymap.on('click', onMapClick);
+    map.on('click', onMapClick);
+}
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+        d > 500  ? '#BD0026' :
+            d > 200  ? '#E31A1C' :
+                d > 100  ? '#FC4E2A' :
+                    d > 50   ? '#FD8D3C' :
+                        d > 20   ? '#FEB24C' :
+                            d > 10   ? '#FED976' :
+                                '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
 }
