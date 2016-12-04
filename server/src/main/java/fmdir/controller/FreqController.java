6fc009d77ca1e49df592ffc1504aa64671fe7d23
Peter Nancke <peter.nancke@gmx.de>
2016-12-04 20:19:52 +0100

    Set RequestParams to optional

--- server/src/main/java/fmdir/controller/FreqController.java
+++ server/src/main/java/fmdir/controller/FreqController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 
 @RestController
 public class FreqController {
@@ -27,11 +25,8 @@ public class FreqController {
     }
 
     @RequestMapping(value = "/freqs", method = RequestMethod.GET)
-    public String getFreqs(@RequestParam Map<String, String> requestParams) {
-
-        Integer start = Integer.valueOf(requestParams.get("start"));
-        Integer end = Integer.valueOf(requestParams.get("end"));
-
+    public String getFreqs(@RequestParam(value = "start", required = false, defaultValue = "1900") Integer start,
+                           @RequestParam(value = "end", required = false, defaultValue = "2100") Integer end) {
         //TODO get real frequency data from database
         JSONObject response = new JSONObject();
         JSONArray countries = new JSONArray();
