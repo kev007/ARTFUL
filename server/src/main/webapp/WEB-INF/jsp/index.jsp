05551dc501b14b2030e8d52dab8e2730d009fc15
Kevin Shrestha <kev_shrestha@hotmail.com>
2016-12-05 12:23:28 +0100

    #10 updated GUI. added legend and country reference frequency placeholder.

--- server/src/main/webapp/WEB-INF/jsp/index.jsp
+++ server/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,6 +2,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <html lang="en">
+<title>Interactive Country Reference Frequency Choropleth Map</title>
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- server/src/main/webapp/resources/js/app.js
+++ server/src/main/webapp/resources/js/app.js
@@ -1,11 +1,12 @@
 var geojson;
 var map;
+var info;
 
 function initLeafletMap() {
     map = L.map('mapid').setView([51.505, -0.09], 3);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
-        maxZoom: 18,
+        maxZoom: 7,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -18,11 +19,54 @@ function initLeafletMap() {
 
     var popup = L.popup();
     function onMapClick(e) {
-        popup
-            .setLatLng(e.latlng)
-            .setContent("You clicked the map at " + e.latlng.toString())
-            .openOn(map);
+        console.log("You clicked on: " + e.latlng.toString());
+        // popup
+        //     .setLatLng(e.latlng)
+        //     .setContent("You clicked the map at " + e.latlng.toString())
+        //     .openOn(map);
     }
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+            // grades = [1000, 500, 200, 100, 50, 20, 10, 0],
+            labels = [];
+
+        div.innerHTML += '<h3>Legend</h3>';
+
+        // loop through our density intervals and generate a label with a colored square for each interval
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(map);
+
+    info = L.control();
+
+    info.onAdd = function (map) {
+        this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+        this.update();
+        return this._div;
+    };
+
+    // method that we will use to update the control based on feature properties passed
+    //TODO: properly recieve the passed frequency
+    info.update = function (props) {
+        this._div.innerHTML = '<h2>Interactive Country Reference Frequency Choropleth Map</h2>' +
+            '<h3>Number of references to {SELECTED COUNTRY}</h3>' +  (props ?
+            '<b>' + props.name + '</b><br />' + props.freq
+                : 'Hover over a country');
+    };
+
+    info.addTo(map);
 
     map.on('click', onMapClick);
 }
@@ -93,20 +137,26 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+
+    info.update(layer.feature.properties); //TODO: pass the correct properties
 }
 
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
+
+    info.update();
 }
 
 
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
+        mouseout: resetHighlight//,
+        // click: zoomToFeature
     });
 }
+
+
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
@@ -115,7 +165,10 @@ function getFreqs(beginYear, endYear, map) {
     httpGetAsync('/freqs?start=' + beginYear + '&end=' + endYear, function (response) {
         var countryFreq = JSON.parse(response);
         var mergedData = mergeCountryFreq(countryFreq['countries'], countryData);
-        geojson = L.geoJson(mergedData, {style: style, onEachFeature: onEachFeature}).addTo(map);
+        geojson = L.geoJson(mergedData, {
+            style: style,
+            onEachFeature: onEachFeature
+        }).addTo(map);
     });
 }
 
