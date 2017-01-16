0694d258c80146ec4047b1e8332a20100af38980
Daniel Obraczka <daniel@obraczka.de>
2017-01-16 10:35:48 +0100

    Added Sparql query for politicians and result

--- leipzigCorporaParser/src/main/java/fmdir/SparqlQueryTools.java
+++ leipzigCorporaParser/src/main/java/fmdir/SparqlQueryTools.java
@@ -11,6 +11,7 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.query.Syntax;
 import org.apache.jena.rdf.model.Model;
 
@@ -22,6 +23,7 @@ import org.apache.jena.rdf.model.Model;
  */
 public class SparqlQueryTools {
 
+	public static final String parRegex = "\\(([^)]+)\\)";
     public static final String DBPEDIA_ENDPOINT = "http://dbpedia.org/sparql";
     public static final String GET_ALL_CITIES_QUERY_HEAD = "PREFIX dbo: <http://dbpedia.org/ontology/>"
     												+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
@@ -39,8 +41,45 @@ public class SparqlQueryTools {
     public static final String GET_ALL_CITIES_TOWN = "WHERE { ?city rdf:type dbo:Town ; rdfs:label ?citylabel ; dbo:country ?locatedIn ; dbo:populationTotal  ?population ; dbo:wikiPageID ?id . ?locatedIn rdfs:label ?country . FILTER (?population >= 500000 ) FILTER (lang(?country)='en')}"; 
     public static final String GET_ALL_CITIES_UMBEL = "WHERE { ?city rdf:type umbel:City ; rdfs:label ?citylabel ; dbo:country ?locatedIn ; dbo:populationTotal  ?population ; dbo:wikiPageID ?id . ?locatedIn rdfs:label ?country . FILTER (?population >= 500000 ) FILTER (lang(?country)='en')}"; 
 //    												+ "WHERE { ?city rdf:type dbo:City .}"; 
+    
+    
+    public static final String GET_POLITICIANS_QUERY = "PREFIX dbo: <http://dbpedia.org/ontology/> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> SELECT DISTINCT ?id ?leadername ?countrylabel lang(?leadername) WHERE { ?country dbo:leader ?leader ; rdfs:label ?countrylabel . ?x dbo:country ?country . ?leader dbo:wikiPageID ?id ; rdfs:label ?leadername .  FILTER (lang(?countrylabel)='en')    }ORDER BY ?leadername";
+    
     public static void main(String[] args){
-    		
+    	getPoliticianData();
+    }
+    
+    public static void getPoliticianData(){
+    	String resultStr = "";
+		int c = 0;
+		HashSet<QuerySolution> finalRes = new HashSet<QuerySolution>();
+		ResultSet results = querySelect(GET_POLITICIANS_QUERY, DBPEDIA_ENDPOINT, null, null);
+		while(results.hasNext()){
+			finalRes.add(results.next());
+		}
+		for(QuerySolution qs: finalRes){
+			c++;
+			String id = qs.getLiteral("?id").getString();
+			String leadertmp = qs.getLiteral("?leadername").getString();
+			//Clean city labels like "Wayanad (district)" by removing everything enclosed in brackets
+			String leaderlabel = leadertmp.replaceAll(parRegex,"").trim();
+			String countrytmp = qs.getLiteral("?countrylabel").getString();
+			String country = countrytmp.replaceAll(parRegex, "").trim();
+			String language = qs.getLiteral("?callret-3").getString();
+			resultStr += id + "\t" + leaderlabel + "\t" + country + "\t" + language + "\n";
+		}
+		System.out.println("\n\n == " + c + " == \n\n");
+//		System.out.println(resultStr);
+    		try{
+    			PrintWriter writer = new PrintWriter(new File("leipzigCorporaParser/resources/translations/politicians.csv"));
+    			writer.write(resultStr);
+    			writer.close();
+    		}catch(Exception e){
+    			e.printStackTrace();
+    		}
+    }
+    
+    public static void getCityData(){
     	String resultStr = "";
 		int c = 0;
 		//Contains all results from settlements, towns, cities
@@ -74,8 +113,9 @@ public class SparqlQueryTools {
 			String id = qs.getLiteral("?id").getString();
 			String citytmp = qs.getLiteral("?citylabel").getString();
 			//Clean city labels like "Wayanad (district)" by removing everything enclosed in brackets
-			String citylabel = citytmp.replaceAll("\\(.+\\)","");
-			String locatedIn = qs.getLiteral("?country").getString();
+			String citylabel = citytmp.replaceAll(parRegex,"").trim();
+			String countrytmp = qs.getLiteral("?country").getString();
+			String locatedIn = countrytmp.replaceAll(parRegex, "").trim();
 			String language = qs.getLiteral("?callret-3").getString();
 			resultStr += id + "\t" + citylabel + "\t" + locatedIn + "\t" + language + "\n";
 		}
