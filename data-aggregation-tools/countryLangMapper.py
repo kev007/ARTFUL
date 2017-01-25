add5a37cb908686c798a8bee26b19a53cd7a8f30
Peter Nancke <peter.nancke@gmx.de>
2017-01-25 13:33:16 +0100

    Transform country codes to lower case

--- data-aggregation-tools/countryLangMapper.py
+++ data-aggregation-tools/countryLangMapper.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import collections
 import configparser
 import csv
 import json
@@ -8,28 +7,6 @@ import sqlite3
 
 config = configparser.ConfigParser()
 config.read('config.conf')
-
-
-class CaseInsensitiveDict(collections.Mapping):
-    def __init__(self, d):
-        self._d = d
-        self._s = dict((k.lower(), k) for k in d)
-
-    def __contains__(self, k):
-        return k.lower() in self._s
-
-    def __len__(self):
-        return len(self._s)
-
-    def __iter__(self):
-        return iter(self._s)
-
-    def __getitem__(self, k):
-        return self._d[self._s[k.lower()]]
-
-    def actual_key_case(self, k):
-        return self._s.get(k.lower())
-
 
 connection = sqlite3.connect(config['database']['file'])
 cursor = connection.cursor()
@@ -42,22 +19,26 @@ country_codes = {}
 with open('country-code.csv', mode='r') as infile:
     reader = csv.reader(infile, delimiter=';')
     for rows in reader:
-        country_codes.update({rows[1]: rows[0] for rows in reader})
+        country_codes.update({rows[1].lower(): rows[0].lower() for rows in reader})
 
 with open('country-code.csv', mode='r') as infile:
     reader = csv.reader(infile, delimiter=';')
     for rows in reader:
-        country_codes.update({rows[2]: rows[0] for rows in reader})
+        country_codes.update({rows[2].lower(): rows[0].lower() for rows in reader})
 
-country_codes_case_insensitive = CaseInsensitiveDict(country_codes)
 mappings = {}
 for corpus in corpora:
     if '-' in corpus:
         split = corpus.split('-')
-        mappings.update({corpus: country_codes_case_insensitive.get(split[1])})
+        mappings.update({corpus: country_codes.get(split[1])})
     else:
-        mappings.update({corpus: country_codes_case_insensitive.get(corpus)})
-print(mappings)
+        mappings.update({corpus: country_codes.get(corpus)})
 
-with open(config['server']['language-mappings-file'], "w+") as result_file:
-    result_file.write(json.dumps(mappings))
+inv_mapping = {v: k for k, v in mappings.items()}
+print(inv_mapping)
+
+with open(config['server']['language-country-mapping'], "w+") as result_file:
+    result_file.write("var language_country_mapping = " + json.dumps(mappings))
+
+with open(config['server']['country-language-mapping'], "w+") as result_file:
+    result_file.write("var country_language_mapping = " + json.dumps(inv_mapping))
