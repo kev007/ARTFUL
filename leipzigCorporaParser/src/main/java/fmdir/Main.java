0194b980c2e3f450f0a62dd69d5ce1a62a9f8eae
Kevin Shrestha <kev_shrestha@hotmail.com>
2017-02-06 17:54:45 +0100

    #31 handled failure case if the corpora size cannot be determined

--- leipzigCorporaParser/src/main/java/fmdir/Main.java
+++ leipzigCorporaParser/src/main/java/fmdir/Main.java
@@ -83,7 +83,7 @@ public class Main {
 
                 System.out.println(ANSI_BLUE + "(" + currentCorpora + "/" + allFreqPaths.size() + ") - " +  year + " " + language + ": " + wordFreq.size() + " words imported in \t\t " + (float)(System.currentTimeMillis()-parseStart)/1000 + " seconds" + ANSI_RESET);
 
-                int corporaSize = 0;
+                int corporaSize;
                 int sizeRank = FileTools.getCorporaSize(path.toString());
                 switch (sizeRank) {
                     case 5: corporaSize = 10000; break;
@@ -92,6 +92,7 @@ public class Main {
                     case 20: corporaSize = 300000; break;
                     case 25: corporaSize = 1000000; break;
                     case 30: corporaSize = 3000000; break;
+                    default: corporaSize = 0; break;
                 }
 
                 DatabaseTools.fillDatabase(translations, wordFreq, year, language, corporaSize);
