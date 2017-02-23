579266311b1a56848e28fad898a9aa861da8ae8b
Peter Nancke <peter.nancke@gmx.de>
2017-02-23 20:54:43 +0100

    Add missing deployment config

--- server/src/main/java/artful/Server.java
+++ server/src/main/java/artful/Server.java
@@ -2,10 +2,20 @@ package artful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Server {
+public class Server extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class (via @SpringBootApplication)
+        // we actually don't need to override this method.
+        return application;
+    }
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Server.class, args);
