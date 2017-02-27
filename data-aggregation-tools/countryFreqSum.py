183383e42cc86257119b56c9fcdc13168b81c4f2
Peter Nancke <peter.nancke@gmx.de>
2017-02-27 11:40:49 +0100

    Add column for country codes

--- data-aggregation-tools/countryFreqSum.py
+++ data-aggregation-tools/countryFreqSum.py
@@ -2,6 +2,7 @@
 
 import configparser
 import sqlite3
+import csv
 
 config = configparser.ConfigParser()
 config.read('config.conf')
@@ -19,24 +20,65 @@ print("Deleting records from country_freq...")
 deleteRecords = "DELETE FROM `country_freq`"
 cursor.execute(deleteRecords)
 connection.commit()
+
+corporaQuery = "SELECT DISTINCT corpus FROM freq;"
+cursor.execute(corporaQuery)
+corpora = [i[0] for i in (list(cursor.fetchall()))]
+
+# Get country language mapping
+country_codes_short = {}
+country_codes_middle = {}
+country_codes_long = {}
+with open('country-code.csv', mode='r') as infile:
+    reader = csv.reader(infile, delimiter=';')
+    for rows in reader:
+        country_codes_short.update({rows[1].lower(): rows[0].lower() for rows in reader})
+
+with open('country-code.csv', mode='r') as infile:
+    reader = csv.reader(infile, delimiter=';')
+    for rows in reader:
+        country_codes_middle.update({rows[2].lower(): rows[0].lower() for rows in reader})
+
+with open('country-code.csv', mode='r') as infile:
+    reader = csv.reader(infile, delimiter=';')
+    for rows in reader:
+        if len(rows) > 3 and rows[3] != '':
+            country_codes_long.update({rows[3].lower(): rows[0].lower()})
+
+mappings = {}
+for corpus in corpora:
+    if corpus not in mappings and corpus in country_codes_short:
+        corpus_ = {corpus: country_codes_short.get(corpus)}
+        mappings.update(corpus_)
+    if corpus not in mappings and corpus in country_codes_middle:
+        get_corpus_ = {corpus: country_codes_middle.get(corpus)}
+        mappings.update(get_corpus_)
+    if corpus not in mappings and corpus in country_codes_long:
+        long_get_corpus_ = {corpus: country_codes_long.get(corpus)}
+        mappings.update(long_get_corpus_)
+
+inv_mapping = {v: k for k, v in mappings.items()}
+
 for locatedIn in locatedIns:
-    print("Inserting frequencies for " + locatedIn + "...")
-    for year in years:
-        corporaSizeQuery = "SELECT round(sum(size) * 1.0 / count(*)) FROM corpora " \
-                           "WHERE id IN " \
-                           "(SELECT DISTINCT corporaID FROM freq f, translation t " \
-                           "WHERE f.translation_id = t.id AND t.located_in = '{}' AND f.year = {})" \
-            .format(locatedIn, year)
-        cursor.execute(corporaSizeQuery)
-        avgCorporaSize = cursor.fetchone()[0]
-        if avgCorporaSize is None:
-            continue
-        insert = "INSERT INTO `country_freq`(country, freq, year, avg_corpora_size)" \
-                 " SELECT located_in AS country, sum(freq) AS freq, f.year, {} as avgCorporaSize" \
-                 " FROM freq f, translation t" \
-                 " WHERE f.translation_id = t.id AND t.located_in = '{}' AND f.year = {};" \
-            .format(avgCorporaSize, locatedIn, year)
-        cursor.execute(insert)
-        connection.commit()
+    # print("Inserting frequencies for " + locatedIn + "...")
+    if locatedIn.lower() not in inv_mapping:
+        print("No mapping found for: " + locatedIn)
+    else:
+        for year in years:
+            lang = inv_mapping[locatedIn.lower()]
+            avgIngoingReferencesCorpusSizeQuery = "SELECT round(sum(size) * 1.0 / count(*)) FROM corpora " \
+                                                  "WHERE lang = '{}' AND year = {}" \
+                .format(lang, year)
+            cursor.execute(avgIngoingReferencesCorpusSizeQuery)
+            avgIngoingReferencesCorpusSize = cursor.fetchone()[0]
+            if avgIngoingReferencesCorpusSize is None:
+                continue
+            insert = "INSERT INTO `country_freq`(country, freq, year, avg_corpora_size)" \
+                     " SELECT located_in AS country, sum(freq) AS freq, f.year, {} as avgCorporaSize" \
+                     " FROM freq f, translation t" \
+                     " WHERE f.translation_id = t.id AND t.located_in = '{}' AND f.year = {};" \
+                .format(avgIngoingReferencesCorpusSize, locatedIn, year)
+            cursor.execute(insert)
+            connection.commit()
 
 connection.close()
