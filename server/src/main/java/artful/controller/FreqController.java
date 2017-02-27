7d4f38b3ba7bbfa7ff6bfd79f22ab3102f8075cb
Peter Nancke <peter.nancke@gmx.de>
2017-02-27 15:19:32 +0100

    Generate ingoing and outgoing avg corpus sizes

--- server/src/main/java/artful/controller/FreqController.java
+++ server/src/main/java/artful/controller/FreqController.java
@@ -55,19 +55,25 @@ public class FreqController {
 
         List<Object[]> freqs = repository.findAllByYearBetween(start, end);
 
-        final Integer[] maxCorporaSize = {0};
+        final Integer[] maxCorporaSize = {0, 0};
         freqs.forEach((record) -> {
             JSONObject currCountry = new JSONObject();
             currCountry.put("name", (String) record[0]);
             currCountry.put("frequency", (Long) record[1]);
-            Integer currCorporaSize = (Integer) record[2];
-            currCountry.put("avgCorporaSize", currCorporaSize);
-            countries.put(currCountry);
-            if(currCorporaSize > maxCorporaSize[0]){
-                maxCorporaSize[0] = currCorporaSize;
+            Integer currCorporaSizeIngoing = (Integer) record[2];
+            currCountry.put("avgCorporaSizeIngoing", currCorporaSizeIngoing);
+            if(currCorporaSizeIngoing > maxCorporaSize[0]){
+                maxCorporaSize[0] = currCorporaSizeIngoing;
             }
+            Integer currCorporaSizeOutgoing = (Integer) record[3];
+            currCountry.put("avgCorporaSizeOutgoing", currCorporaSizeOutgoing);
+            if(currCorporaSizeOutgoing > maxCorporaSize[1]){
+                maxCorporaSize[1] = currCorporaSizeOutgoing;
+            }
+            countries.put(currCountry);
         });
-        response.put("max corpora size", maxCorporaSize[0]);
+        response.put("max corpora size ingoing", maxCorporaSize[0]);
+        response.put("max corpora size outgoing", maxCorporaSize[1]);
         return String.valueOf(response);
     }
 }
